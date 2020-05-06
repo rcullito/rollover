cbbdddab0fdfee3824d7deacdf6543801adbd11c
Rob Culliton <rob.culliton@gmail.com>
2020-05-06 12:23:32 -0400

    add expo-sensors library, import gyroscope, and simple console log to make sure it is available on physical android device

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Gyroscope } from 'expo-sensors';
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
 
-const intro = 'Welcome to Rollover friend of Rob';
+const intro = 'Welcome to Rollover friend of Rob. You are wise';
+
+let gyroAboard = Gyroscope.isAvailableAsync()
+    .then( (val) => console.log("gyroscope available: ", val));
 
 export default function App() {
   return (
