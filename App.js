d1c8040ed7b309ef6119f3d15b408832b506ca6a
Rob Culliton <rob.culliton@gmail.com>
2020-05-15 11:35:44 -0400

    clean up some of the original boilerplate. unvalidated so make sure to test :)

--- App.js
+++ App.js
@@ -2,22 +2,11 @@ import * as React from 'react';
 import { Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import startDeviceMotion from "./logic";
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
-
 const intro = 'Rollover: an app designed to help make you more aware of your movements during sleep';
-
-// make a button to start device motion from
-// this means we would probably also need a stop function :)
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
       <Text style={styles.instructions}>{intro}</Text>
 
        <TouchableOpacity
@@ -35,11 +24,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
   },
   instructions: {
     textAlign: 'center',
