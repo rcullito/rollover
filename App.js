3045bc7f5369b31a3ac2fd3cf29a491be0b629ac
Rob Culliton <rob.culliton@gmail.com>
2020-05-16 15:06:42 -0400

    ok we have successfully split out styles into their own stylesheet. that is something

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { ImageBackground, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import styles from './styles.js';
 import { startDeviceMotion, stopDeviceMotion } from "./logic";
 
 const intro = 'Rollover: an app designed to help make you more aware of your movements during sleep';
@@ -13,7 +14,6 @@ class MotionButton extends React.Component {
       inmotion: false,
     };
   }
-
 
     // TODO work with the subscription from the individual
     // listener rather than having to remove all listeners
@@ -34,7 +34,6 @@ class MotionButton extends React.Component {
   render() {
       return (
               <TouchableOpacity onPress={this.updateInmotion}
-          // TODO get these styles to the bottom, and then ultimately split them out into their own file
       style={styles.motionButton}>
               <Text style={styles.motionButtonText}>
               {this.state.inmotion ? 'Stop motion sensor' : 'Start motion sensor'}
@@ -56,25 +55,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-      width: null,
-      height: null,
-  },
-  instructions: {
-    textAlign: 'center',
-      color: '#fff',
-      marginBottom: 15
-  },
-    motionButton: {
-        backgroundColor: 'blue'
-        
-    },
-    motionButtonText: {
-        fontSize: 20,
-        color: '#fff'
-    }
-});
---
+++ styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+      width: null,
+      height: null,
+  },
+  instructions: {
+    textAlign: 'center',
+      color: '#fff',
+      marginBottom: 15
+  },
+    motionButton: {
+        backgroundColor: 'blue'
+        
+    },
+    motionButtonText: {
+        fontSize: 20,
+        color: '#fff'
+    }
+});
+
+export default styles;
