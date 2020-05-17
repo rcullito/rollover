ba008c331e2b62d59bb22aa1bf524543edf83a72
Rob Culliton <rob.culliton@gmail.com>
2020-05-17 13:46:24 -0400

    the beginnings of something truly great

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { ImageBackground, Text, View } from 'react-native';
 import styles from './styles.js';
 import MotionButton from './MotionButton.js';
+import './waves.js'
 
 const intro = 'Rollover: an app designed to help make you more aware of your movements during sleep';
 
@@ -12,6 +13,7 @@ export default function App() {
           <View style={styles.container}>
           <Text style={styles.instructions}>{intro}</Text>
           <MotionButton></MotionButton>
+          <canvas id="waves" width="200" height="100"></canvas>
           </View>
           </ImageBackground>
   );
