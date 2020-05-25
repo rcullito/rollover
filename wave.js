31bc11f5a9ebabcf7a2c3553cf7450b3c0fb5865
Rob Culliton <rob.culliton@gmail.com>
2020-05-25 10:29:24 -0400

    robert, someone is watching out for you. changing T to windowWidth smoothed everything out

--- wave.js
+++ wave.js
@@ -1,7 +1,11 @@
 import * as React from 'react';
+import { Dimensions } from 'react-native';
 import ForkedWave from './ForkedWave.js';
 import styles from './styles.js';
 
+
+const windowWidth = Dimensions.get('window').width;
+console.log(windowWidth);
 
 class RobWave extends React.Component {
     render () {
@@ -11,9 +15,7 @@ class RobWave extends React.Component {
             style={styles.wave}
             H={15}
             waveParams={[
-                {A: 10, T: 300, stroke: '#62c2ff', fill: 'none'},
-                {A: 12, T: 300, stroke: '#0087dc', fill: 'none'},
-                {A: 14, T: 300, stroke: '#1aa7ff', fill: 'none'},
+                {A: 10, T: windowWidth, stroke: '#62c2ff', fill: 'none'},
             ]}
             animated={true}
                 />
