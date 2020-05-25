c83f54defa1198a39149d5a8f2a9c160229dd14a
Rob Culliton <rob.culliton@gmail.com>
2020-05-25 09:42:57 -0400

    golf calp for updating the stroke of the local svg fork

--- wave.js
+++ wave.js
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import Wave from 'react-native-waveview';
+import ForkedWave from './ForkedWave.js';
 import styles from './styles.js';
 
 
 class RobWave extends React.Component {
     render () {
         return (
-                <Wave
+                <ForkedWave
             ref={ref=>this._waveRect = ref}
             style={styles.wave}
             H={15}
             waveParams={[
-                {A: 10, T: 300, fill: '#62c2ff'},
+                {A: 10, T: 300, stroke: '#62c2ff', fill: 'none'},
             ]}
             animated={true}
                 />
