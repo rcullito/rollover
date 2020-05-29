a1db9a8bfd297a7d8d172715470224eaae6bec29
Rob Culliton <rob.culliton@gmail.com>
2020-05-29 09:34:10 -0400

    give our wave component the full react-redux bindings

--- wave.js
+++ wave.js
@@ -2,13 +2,27 @@ import * as React from 'react';
 import { TouchableHighlight, Dimensions } from 'react-native';
 import ForkedWave from './ForkedWave.js';
 import styles from './styles.js';
-
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as motionActions from './src/actions/motionActions';
 
 const windowWidth = Dimensions.get('window').width;
-console.log(windowWidth);
 
 class RobWave extends React.Component {
 
+    // get a ref here and then use this.props
+
+    constructor(props) {
+        super(props);
+        this.myRef = React.createRef();
+    }
+
+    componentDidUpdate(prevProps) {
+        console.log('updated');
+        console.log(prevProps);
+        console.log(this.props);
+    }
+    
     render () {
         return (
                 <ForkedWave
@@ -18,13 +32,26 @@ class RobWave extends React.Component {
             waveParams={[
                 {A: 10, T: windowWidth, stroke: '#8a2be2', fill: 'none'},
             ]}
+            rob={this.props.motion}
             animated={false}
                 />
         )
     }
 }
 
-export default RobWave;
+function mapStateToProps(state) {
+  return {
+    motion: state.motion
+  };
+}
 
+function mapDispatchToProps(dispatch) {
+  return {
+    motionActions: bindActionCreators(motionActions, dispatch)
+  };
+}
 
-
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(RobWave);
