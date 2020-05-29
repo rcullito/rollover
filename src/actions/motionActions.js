6d7361d6030f5a181d326b1a2de09f00ff962e5e
Rob Culliton <rob.culliton@gmail.com>
2020-05-29 08:53:55 -0400

    flush out initial state and full suite of actions more

--- src/actions/motionActions.js
+++ src/actions/motionActions.js
@@ -8,3 +8,12 @@ export function startMotion () {
 export function stopMotion () {
     return {type: types.STOP_MOTION, motion: 'stopped'}
 }
+
+export function startVibration () {
+    return {type: types.START_VIBRATION, motion: 'vibrating'}
+}
+
+// TODO this may need a state of reset, but see how it goes
+export function stopVibration () {
+    return {type: types.STOP_VIBRATION, motion: 'started'}
+}
--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,3 +1,3 @@
 export default {
-  motion: []
+  motion: 'stopped'
 };
