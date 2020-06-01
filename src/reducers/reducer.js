c69b70f69db2c0a0bd9b2da983a7fdf8c34edb52
Rob Culliton <rob.culliton@gmail.com>
2020-05-31 21:16:13 -0400

    I dont know about much else, but that is a damn respectable reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -4,14 +4,13 @@ import {START_MOTION, STOP_MOTION, START_VIBRATION, STOP_VIBRATION} from '../act
 export default function motion(state = initialState, action) {
 
   switch (action.type) {
-    case START_MOTION:
-      return action;
-    case STOP_MOTION:
-      return action;
-    case START_VIBRATION:
-      return action;
+  case START_MOTION:
+      return Object.assign({hasBeenStarted: true, motion: action.motion}, state);
+  case STOP_MOTION:
+      return Object.assign({motion: action.motion}, state);
+  case START_VIBRATION:
   case STOP_VIBRATION:
-      return action;
+      return Object.assign({vibrating: action.vibrating}, state);
     default:
       return state;
   }
